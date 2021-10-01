e2bebeabb610d82c5a0ba1d9290786301498cda3
ya-vasya <vasyl.stashchuk.pz.2018@lpnu.ua>
2021-10-01 17:21:57 +0300

    Added routing and basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'posts', component: PostsComponent },
+  {path: 'profile', component: ProfileComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
